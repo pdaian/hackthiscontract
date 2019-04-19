d309da3a4c2c5210d767d7ed90bbb36767142757
Aayushi Jain <asjain237@gmail.com>
2019-04-19 13:53:32 -0400

    wierd obscure bug in 06 hopfully this fixes

--- challenges/06_multisig_magic.py
+++ challenges/06_multisig_magic.py
@@ -8,13 +8,14 @@ import util
 DEPOSIT_AMT = 5000000000000000
 class Contract(icontract.IContract):
     def setup(self):
-        bytecode, abi = util.get_bytecode_abi("06_wallet_library")
+        contract_name = "06_wallet_library"
+        bytecode, abi = util.get_bytecode_abi(contract_name)
         wallet_library_contract = self.eweb3._web3.eth.contract(abi=abi, bytecode=bytecode)
         wallet_library_contract_address = None
 
         # deploy wallet library contract
         tx_hash = wallet_library_contract.constructor().transact({'from': constants.DEPLOY_FROM_ADDRESS, 'gas': 2000000})
-        tx_receipt = self.eweb3._web3.eth.waitForTransactionReceipt(tx_hash, 180)
+        tx_receipt = self.eweb3._web3.eth.waitForTransactionReceipt(tx_hash)
         wallet_library_contract_address = tx_receipt.contractAddress
 
         # set wallet library's address in the wallet contract
@@ -23,7 +24,7 @@ class Contract(icontract.IContract):
             "from": constants.DEPLOY_FROM_ADDRESS,
             "value": 0,
             "gas": 200000})
-        wallet_library_tx_receipt = self.eweb3._web3.eth.waitForTransactionReceipt(wallet_library_tx_hash, 180)
+        wallet_library_tx_receipt = self.eweb3._web3.eth.waitForTransactionReceipt(wallet_library_tx_hash)
 
         # deposit ether in the wallet contract
         deposit_tx_hash = self.eweb3._web3.eth.sendTransaction({
@@ -31,11 +32,11 @@ class Contract(icontract.IContract):
             "to": self.contract_object.address,
             "value": DEPOSIT_AMT,
             "gas": 500000})
-        deposit_tx_receipt = self.eweb3._web3.eth.waitForTransactionReceipt(deposit_tx_hash, 180)
+        deposit_tx_receipt = self.eweb3._web3.eth.waitForTransactionReceipt(deposit_tx_hash)
 
     def has_been_hacked(self):
         contract_bal = self.eweb3._web3.eth.getBalance(self.contract_object.address)
-        #print(contract_bal)
+        print(contract_bal)
         if contract_bal < DEPOSIT_AMT:
             return True
         else:
